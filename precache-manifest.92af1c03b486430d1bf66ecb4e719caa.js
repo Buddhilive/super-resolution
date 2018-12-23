dc9b49d18eb032053e9b0c004b1a1842a6a403da
Greg French <gregfrench26@gmail.com>
2018-12-23 07:48:03 -0500

    Updates

---
+++ precache-manifest.92af1c03b486430d1bf66ecb4e719caa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a31625ea552e36b6ee3a",
+    "url": "/super-resolution/static/css/main.e90f04af.chunk.css"
+  },
+  {
+    "revision": "a31625ea552e36b6ee3a",
+    "url": "/super-resolution/static/js/main.a31625ea.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/super-resolution/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "4f9c75ca32e65b899f5a",
+    "url": "/super-resolution/static/js/runtime~main.4f9c75ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/super-resolution/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "596f32c3ff5bb3d3a5a3ad8142d62407",
+    "url": "/super-resolution/index.html"
+  }
+];
