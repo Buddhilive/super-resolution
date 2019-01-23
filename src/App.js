a2cb586520f2d74c59ad9b956a170ad710bb1d31
Greg French <gregfrench26@gmail.com>
2019-01-22 21:23:13 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ import { css } from '@emotion/core';
 // First way to import
 import { ClipLoader } from 'react-spinners';
 
-const override = css`
-    display: block;
-    margin: 0 auto;
-    border-color: red;
-`;
-
 export default class App extends Component {
   constructor(props) {
     super(props)
@@ -38,14 +32,13 @@ export default class App extends Component {
         <p>Drag and drop images to increase the size of an image by 2x without losing quality.</p>
         <div className='sweet-loading'>
         <ClipLoader
-          css={override}
           sizeUnit={"px"}
           size={150}
           color={'#123abc'}
           loading={this.state.loading}
         />
         {this.state.loading &&
-        <div style={{'text-align': 'center'}}>Weights are being initialized. Please wait.</div>
+        <div style={{textAlign: 'center'}}>Weights are being initialized. Please wait.</div>
         }
       </div>
         {!this.state.loading &&
