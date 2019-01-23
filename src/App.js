6004c71f7547d42a6ae956870282898af482738d
Greg French <gregfrench26@gmail.com>
2019-01-22 21:13:11 -0500

    Added loading bar weight initialization

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import * as tf from '@tensorflow/tfjs';
 import DropImage from './components/DropImage'
 import './App.css';
 
+import { css } from '@emotion/core';
+// First way to import
+import { ClipLoader } from 'react-spinners';
+
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+`;
+
 export default class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-        model: ''
+      loading: true,
+      model: ''
     }
 
     this.loadModel()
@@ -17,7 +28,7 @@ export default class App extends Component {
   async loadModel() {
     let model = await tf.loadModel('./model/model.json');
 
-    this.setState({model: model})
+    this.setState({model: model, loading: false})
   }
 
   render() {
@@ -25,7 +36,21 @@ export default class App extends Component {
       <div className="App">
         <h1>Image Super Resolution</h1>
         <p>Drag and drop images to increase the size of an image by 2x without losing quality.</p>
-        <DropImage model={this.state.model} />
+        <div className='sweet-loading'>
+        <ClipLoader
+          css={override}
+          sizeUnit={"px"}
+          size={150}
+          color={'#123abc'}
+          loading={this.state.loading}
+        />
+        {this.state.loading &&
+        <div style={{'text-align': 'center'}}>Weights are being initialized. Please wait.</div>
+        }
+      </div>
+        {!this.state.loading &&
+          <DropImage model={this.state.model} />
+        }
       </div>
     );
   }
